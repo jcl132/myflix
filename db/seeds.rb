5c061e3adeef91efb8b9b0a9e8bf931a8d84506f
jcl132 <jcl132>
2014-11-12 06:24:43 +0000

    add video model, controller, migration and built the home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "Monk", description: "Some show about monks?", small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
