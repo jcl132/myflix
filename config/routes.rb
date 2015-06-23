29c9d9ae759dcb78689e5dba43b776d34ad12f48
jcl132 <jcl132>
2015-06-23 00:35:34 +0000

    implement search recent videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  root to: 'videos#index'
-  get '/home', to: 'videos#index'
-  get '/genre', to: 'categories#show'
+  root to: 'pages#front'
+  get 'register', to: 'users#new'
+  get 'home', to: 'videos#index'
+  get 'genre', to: 'categories#show'
+  get 'sign_in', to: 'sessions#new'
   resources :videos do
     collection do
       get 'search'
     end
   end
+  resources :users, only: [:create]
 end
